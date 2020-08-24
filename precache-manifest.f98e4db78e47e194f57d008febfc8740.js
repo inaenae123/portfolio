6a44c4e737f6dc32c61db22abe94d60bd7bdc909
Avery Inae Park <ipark71@gatech.edu>
2020-08-24 15:35:00 -0400

    Updates

---
+++ precache-manifest.f98e4db78e47e194f57d008febfc8740.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71757d62b48e5f5ee7bcac3893719fc7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b3f8caa661d234ae5985",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7916c35bc88c62bad6d3",
+    "url": "/portfolio/static/css/main.464aacc2.chunk.css"
+  },
+  {
+    "revision": "b3f8caa661d234ae5985",
+    "url": "/portfolio/static/js/2.1ef238b3.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/portfolio/static/js/2.1ef238b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7916c35bc88c62bad6d3",
+    "url": "/portfolio/static/js/main.d7a5ce96.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7b0cdf6e99328d1d38bde030d4dd8813",
+    "url": "/portfolio/static/media/AdobeClean-Regular.7b0cdf6e.ttf"
+  },
+  {
+    "revision": "ff5ac0d36bddac9907055da7de1352a6",
+    "url": "/portfolio/static/media/IBM-logo.ff5ac0d3.png"
+  },
+  {
+    "revision": "fe5cf3b198346e18a650338cc0b056e6",
+    "url": "/portfolio/static/media/callforcode-icon.fe5cf3b1.png"
+  },
+  {
+    "revision": "196a7ebe53150c7f8fb308e1656a9aba",
+    "url": "/portfolio/static/media/forever21-icon.196a7ebe.png"
+  },
+  {
+    "revision": "ca5a4bfe77a2c95598707a73194de427",
+    "url": "/portfolio/static/media/picturepearfect-icon.ca5a4bfe.png"
+  }
+]);
